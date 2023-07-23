f66531965764d11fab82300ced79abb1521f9c6e
Михаил <mich.goncharov@gmail.com>
2023-07-23 19:36:32 +0300

    Добавьте файлы проекта.

---
+++ Strategy/Duck.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public abstract class Duck
+    {
+        FlyBehaviour flyBehaviour;
+        QuackBehaviour quackBehaviour;
+
+        public Duck(FlyBehaviour flyBehaviour, QuackBehaviour quackBehaviour)
+        {
+            this.flyBehaviour = flyBehaviour;
+            this.quackBehaviour = quackBehaviour;
+        }
+
+        public void performFly()
+        {
+            flyBehaviour.Fly();
+        }
+
+        public void performQuack()
+        {
+            quackBehaviour.Quack();
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("All duck's can swim!");
+        }
+
+        public void SetFlyBehaviour(FlyBehaviour fb)
+        {
+            flyBehaviour= fb;
+        }
+
+        public void SetQuackBehaviour(QuackBehaviour qb)
+        {
+            quackBehaviour= qb;
+        }
+    }
+}
---
+++ Strategy/Quack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class Quack : QuackBehaviour
+    {
+        void QuackBehaviour.Quack()
+        {
+            Console.WriteLine("Quack!");
+        }
+    }
+}
